c0533b73d79f101d4d4af93f9575891050ca8ec4
The12thAvenger <85712985+The12thAvenger@users.noreply.github.com>
2023-09-25 18:48:52 +0200

    Removed json serialization remnants

--- FbxImporter/ViewModels/FbxSceneDataViewModel.cs
+++ FbxImporter/ViewModels/FbxSceneDataViewModel.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections.ObjectModel;
-using Newtonsoft.Json;
 using ReactiveUI.Fody.Helpers;
 
 namespace FbxImporter.ViewModels;
 
-[JsonObject(MemberSerialization.OptIn)]
 public class FbxSceneDataViewModel : ViewModelBase
 {
-    [JsonProperty]
     [Reactive] public ObservableCollection<FbxMeshDataViewModel> MeshData { get; set; } = new();
 
     [Reactive] public FbxMeshDataViewModel? SelectedMesh { get; set; }
